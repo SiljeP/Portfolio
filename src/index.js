84af543a7ec55b4c99b72a12aec9b38f2f68b2c7
SiljeP <u2440913@rts-365.dk>
2023-11-01 10:34:25 +0100

    hero animation made using typed.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import "./style.scss"
+import "./scripts/hero"
 
 
 
---
+++ src/scripts/hero.js
@@ -0,0 +1,10 @@
+import Typed from 'typed.js'
+
+var typed = new Typed(".auto-type", {
+    strings: [ "Silje", "24 years old", "Danish", "Studying Front-End Development" ],
+    startDelay: 800,
+    backDelay: 500,
+    typeSpeed: 100,
+    backSpeed: 80,
+    
+})
