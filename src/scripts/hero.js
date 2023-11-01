5a29225c90e12038c840ac021daea72c1ddd2a24
SiljeP <u2440913@rts-365.dk>
2023-11-01 11:22:55 +0100

    animation completed on arrow

--- src/scripts/hero.js
+++ src/scripts/hero.js
@@ -1,7 +1,7 @@
 import Typed from 'typed.js'
 
 var typed = new Typed(".auto-type", {
-    strings: [ "Silje", "24 years old", "Danish", "Studying Front-End Development" ],
+    strings: [ "Silje", "24 years old","Studying Front-End Development" ],
     startDelay: 800,
     backDelay: 500,
     typeSpeed: 100,
